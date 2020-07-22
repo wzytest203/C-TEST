dff4ea82d97b7201d7224f3b67ba0eb07346f965
admin <wangzhaoyang@piesat.cn>
2020-07-22 09:03:14 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TESTDD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            int clickCent = int.Parse((String)this.lab.Text); //获取当前点击次数
+            clickCent++;
+            this.lab.Text = clickCent.ToString(); //将点击次数装成字符串，并赋值给conent
+
+
+        }
+
+        private void lab_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
